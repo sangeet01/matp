9aae2474f8ff2fd1166dfa37646e2109749ef999
Sangeet <65709147+sangeet01@users.noreply.github.com>
2025-10-17 22:03:12 +0545

    Update mod.rs

--- rust/core/src/ghost/cover_traffic/mod.rs
+++ rust/core/src/ghost/cover_traffic/mod.rs
@@ -3,3 +3,4 @@
 //! This module provides templates and data for generating plausible-looking cover traffic.
 pub mod json_templates;
 
+pub use json_templates::{find_and_replace_payload, get_random_json_template};
