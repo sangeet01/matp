6538dc305e66991735bd64caf519abee89c407de
Sangeet <65709147+sangeet01@users.noreply.github.com>
2025-10-17 23:03:10 +0545

    Update quantum.rs

--- rust/core/src/crypto/quantum.rs
+++ rust/core/src/crypto/quantum.rs
@@ -9,8 +9,6 @@ use serde::{Serialize, Deserialize};
 
 use super::CryptoError;
 
-// --- Type-safe wrappers for Post-Quantum keys ---
-
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
 pub struct KemPublicKey(Vec<u8>);
 
@@ -29,52 +27,45 @@ pub struct KemCiphertext(Vec<u8>);
 #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
 pub struct Signature(Vec<u8>);
 
-/// A namespace struct for post-quantum operations.
 pub struct Quantum;
 
 impl Quantum {
-    /// Generates a Kyber512 keypair for Key Encapsulation.
     pub fn generate_kem_keys() -> Result<(KemPublicKey, KemSecretKey), CryptoError> {
         let kem = Kem::new(KemAlgorithm::Kyber512).map_err(|_| CryptoError::KdfError)?;
         let (pk, sk) = kem.keypair().map_err(|_| CryptoError::KdfError)?;
         Ok((KemPublicKey(pk.into_vec()), KemSecretKey(sk.into_vec())))
     }
 
-    /// Generates a Dilithium2 keypair for signing.
     pub fn generate_sign_keys() -> Result<(SigVerificationKey, SigSigningKey), CryptoError> {
         let sig = Sig::new(SigAlgorithm::Dilithium2).map_err(|_| CryptoError::KdfError)?;
         let (pk, sk) = sig.keypair().map_err(|_| CryptoError::KdfError)?;
         Ok((SigVerificationKey(pk.into_vec()), SigSigningKey(sk.into_vec())))
     }
 
-    /// Encapsulates a shared secret for a given Kyber public key.
     pub fn kem_encapsulate(pk: &KemPublicKey) -> Result<(Vec<u8>, KemCiphertext), CryptoError> {
         let kem = Kem::new(KemAlgorithm::Kyber512).map_err(|_| CryptoError::KdfError)?;
-        let public_key = kem.public_key_from_bytes(&pk.0).map_err(|_| CryptoError::KdfError)?;
+        let public_key = kem.public_key_from_bytes(&pk.0).ok_or(CryptoError::KdfError)?;
         let (shared_secret, ciphertext) = kem.encapsulate(&public_key).map_err(|_| CryptoError::KdfError)?;
         Ok((shared_secret.into_vec(), KemCiphertext(ciphertext.into_vec())))
     }
 
-    /// Decapsulates a shared secret from a Kyber ciphertext.
     pub fn kem_decapsulate(sk: &KemSecretKey, ciphertext: &KemCiphertext) -> Result<Vec<u8>, CryptoError> {
         let kem = Kem::new(KemAlgorithm::Kyber512).map_err(|_| CryptoError::KdfError)?;
-        let secret_key = kem.secret_key_from_bytes(&sk.0).map_err(|_| CryptoError::KdfError)?;
-        let shared_secret = kem.decapsulate(&secret_key, &ciphertext.0).map_err(|_| CryptoError::DecryptionError)?;
+        let secret_key = kem.secret_key_from_bytes(&sk.0).ok_or(CryptoError::KdfError)?;
+        let shared_secret = kem.decapsulate(&secret_key, ciphertext.0.as_slice()).map_err(|_| CryptoError::DecryptionError)?;
         Ok(shared_secret.into_vec())
     }
 
-    /// Signs a message using a Dilithium signing key.
     pub fn sign(sk: &SigSigningKey, message: &[u8]) -> Result<Signature, CryptoError> {
         let sig = Sig::new(SigAlgorithm::Dilithium2).map_err(|_| CryptoError::KdfError)?;
-        let secret_key = sig.secret_key_from_bytes(&sk.0).map_err(|_| CryptoError::KdfError)?;
+        let secret_key = sig.secret_key_from_bytes(&sk.0).ok_or(CryptoError::KdfError)?;
         let signature = sig.sign(message, &secret_key).map_err(|_| CryptoError::EncryptionError)?;
         Ok(Signature(signature.into_vec()))
     }
 
-    /// Verifies a signature using a Dilithium verification key.
     pub fn verify(pk: &SigVerificationKey, message: &[u8], signature: &Signature) -> Result<(), CryptoError> {
         let sig = Sig::new(SigAlgorithm::Dilithium2).map_err(|_| CryptoError::KdfError)?;
-        let public_key = sig.public_key_from_bytes(&pk.0).map_err(|_| CryptoError::KdfError)?;
-        sig.verify(message, &signature.0, &public_key).map_err(|_| CryptoError::DecryptionError)
+        let public_key = sig.public_key_from_bytes(&pk.0).ok_or(CryptoError::KdfError)?;
+        sig.verify(message, signature.0.as_slice(), &public_key).map_err(|_| CryptoError::DecryptionError)
     }
 }
