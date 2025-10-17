d2b1a0c56dc276d3f8756d39edddd792e831f5ce
Sangeet <65709147+sangeet01@users.noreply.github.com>
2025-10-17 22:05:23 +0545

    Update mod.rs

--- rust/core/src/ghost/strategies/mod.rs
+++ rust/core/src/ghost/strategies/mod.rs
@@ -3,6 +3,7 @@
 //! This module contains the concrete implementations of different embedding strategies,
 //! such as hiding data in JSON or image EXIF metadata.
 pub mod json_api;
-// pub mod exif_image;
+pub mod exif_image;
+
 
 
